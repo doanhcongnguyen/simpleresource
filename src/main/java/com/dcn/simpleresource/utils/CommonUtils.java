a7a2ae2f936d1c8cadf69e5951741c2d1951738d
Doanh Cong Nguyen <doanh.nguyen@dzsi.com>
2020-09-22 11:06:54 +0700

    Get IP address

--- src/main/java/com/dcn/simpleresource/utils/CommonUtils.java
+++ src/main/java/com/dcn/simpleresource/utils/CommonUtils.java
@@ -7,13 +7,13 @@ public class CommonUtils {
     public static String getIP(HttpServletRequest request) {
         String ip = request.getHeader("x-forwarded-for");
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
         }
         String[] ips = ip.split(",");
         return "0:0:0:0:0:0:0:1".equals(ips[0]) ? "127.0.0.1" : ips[0];
